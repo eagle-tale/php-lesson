a918541e0d7f053f5eceefedba9d328cf7a230e0
Washio <washio@e-cri.co.jp>
2023-08-24 23:19:14 +0900

    For the first time I tried to use MVC pattern and it's still halfway

---
+++ classes/Menu.php
@@ -0,0 +1,22 @@
+<?php
+
+// クラス宣言
+class Menu
+{
+
+    // プロパティ
+    protected $defaultMenu_array = [
+        '<li><a href="./user_list.php">ユーザー一覧</a></li>',
+        '<li><a href="./reset_password.php">パスワード再設定</a></li>',
+        '<li><a href="./">TOP</a></li>',
+        '<li><a href="logout.php">ログアウト</a></li>'
+    ];
+
+    // メソッド
+    public function show()
+    {
+        foreach ($this->defaultMenu_array as $menu) {
+            echo $menu;
+        }
+    }
+}
